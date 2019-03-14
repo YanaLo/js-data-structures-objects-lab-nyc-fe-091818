9937e97c069a10bcaef9a7acddec13a126844970
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-14 19:26:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let driver={}
+// driver={
+//   name:"Sam",
+//   address:11 Broadway
+// }
+function updateDriverWithKeyAndValue(driver,key,value){
+  let newDriver={...driver}
+  newDriver[key] = value
+  return newDriver;
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+  driver[key]=value;
+  
+  return driver
+}
+function deleteFromDriverByKey(driver, key){
+  let newDriver={...driver}
+  delete newDriver[key]
+  return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+ delete driver[key]
+ return driver
+  
+}
